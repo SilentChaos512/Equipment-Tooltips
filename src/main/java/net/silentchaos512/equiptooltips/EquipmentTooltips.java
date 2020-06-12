084cd6bd9b69ffcc27000a11dc72bde5fd408540
Vaelzan <vaelzan@evilcult.net>
2020-06-12 10:26:48 +1000

    Prevent Equipment Tooltips from causing "FML Incompatible modded server".
    Signed-off-by: Vaelzan <vaelzan@evilcult.net>

--- src/main/java/net/silentchaos512/equiptooltips/EquipmentTooltips.java
+++ src/main/java/net/silentchaos512/equiptooltips/EquipmentTooltips.java
@@ -2,13 +2,17 @@ package net.silentchaos512.equiptooltips;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,6 +26,9 @@ public class EquipmentTooltips {
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
     public EquipmentTooltips() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () ->
+                Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
         if (FMLEnvironment.dist == Dist.CLIENT) {
             Config.init();
             FMLJavaModLoadingContext.get().getModEventBus().addListener(EquipmentTooltips::onCommonSetup);
